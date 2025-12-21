04cc78a6ab8bd9777f5bb7739ed5e3c3b241c119
rimmachepuxa <ch4epuxa@gmail.com>
2025-12-21 23:15:33 +0300

    lab 9

--- task_3/main.py
+++ task_3/main.py
@@ -19,7 +19,7 @@ if not os.path.exists(filename):
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
-# Счётчик операций (кроме выхода)
+# Счётчик операций 
 operations_count = 0
 
 def allrecords():
